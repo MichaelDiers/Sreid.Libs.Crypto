d088009c39968fe7148d418a2e363b5f493e06fe
MichaelDiers <md@sreid.de>
2025-03-26 23:28:18 +0100

    add tests

---
+++ Sreid.Libs.Crypto/Sreid.Libs.Crypto.Tests/Factory/FactoryServiceCollectionExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Sreid.Libs.Crypto.Tests.Factory;
+
+using Sreid.Libs.Crypto.Factory;
+using Sreid.Libs.Crypto.Tests.Helper;
+
+/// <summary>
+///     Tests of <seealso cref="FactoryServiceCollectionExtensions" />.
+/// </summary>
+public class FactoryServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void TryAddCryptoFactory()
+    {
+        var factory =
+            DependencyHelper.GetRequiredService<ICryptoFactory>(FactoryServiceCollectionExtensions.TryAddCryptoFactory);
+        Assert.NotNull(factory);
+    }
+
+    [Fact]
+    public void TryAddFactory()
+    {
+        var factory =
+            DependencyHelper.GetRequiredService<ICryptoFactory>(FactoryServiceCollectionExtensions.TryAddFactory);
+        Assert.NotNull(factory);
+    }
+}
