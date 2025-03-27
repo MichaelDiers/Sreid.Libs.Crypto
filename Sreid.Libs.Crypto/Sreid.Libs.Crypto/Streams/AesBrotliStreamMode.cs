9ccf0fd3ecb453d713ecc5456c1b6daa3fc813f4
MichaelDiers <md@sreid.de>
2025-03-27 02:53:37 +0100

    add aes-brotli stream

---
+++ Sreid.Libs.Crypto/Sreid.Libs.Crypto/Streams/AesBrotliStreamMode.cs
@@ -0,0 +1,17 @@
+﻿namespace Sreid.Libs.Crypto.Streams;
+
+/// <summary>
+///     The supported modes of the <see cref="AesBrotliStream" />.
+/// </summary>
+public enum AesBrotliStreamMode
+{
+    /// <summary>
+    ///     Compress and encrypt the stream data.
+    /// </summary>
+    Pack,
+
+    /// <summary>
+    ///     Decrypt and decompress the stream data.
+    /// </summary>
+    Unpack
+}
